7b4843594d3ebc428f58b648d9bf9cb3bec16a28
Шабров Альберт <56026935+AlbertShabrov@users.noreply.github.com>
2021-09-11 01:04:45 +0300

    Сделал загрузку файла модели, и сделал функцию для добавления данных в бд

--- db/db.py
+++ db/db.py
@@ -1,25 +1,25 @@
 import psycopg2
 
 
-def connect():
-    connection = psycopg2.connect(
-                        host = "localhost",
-                        database = "wb",
-                        user = "postgres",
-                        password = "postgres")
+def decorated_connect(decorate_function):
+    def connect(*args):
+        connection = psycopg2.connect(
+                            host = "localhost",
+                            database = "wb",
+                            user = "postgres",
+                            password = "postgres")
 
-    cur = connection.cursor()
+        cur = connection.cursor()
+        decorate_function(cur, *args)
+        connection.commit()
+        cur.close()
+        connection.close()
+    return connect
 
-    cur.execute("select * from attributes")
-    rows = cur.fetchall()
-
-    for r in rows:
-        print(f"id {r[0]} name {r[1]} icon {r[2]} moduleName {r[3]}")
-
-    cur.close()
-
-    connection.close()
+@decorated_connect
+def add_custom_models(cur, *args):
+    cur.execute("call public.addCustomModels(%s, %s, %s)", (args[0], args[1], args[2]))
 
 
-if __name__ == "__main__":
-    connect()
+add_custom_models('main.py', 'path', 1)
+
